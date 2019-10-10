2635ccd3655a4814f267164afa53b43ef7374fe1
mark.sauer.utley <mark.sauer.utley@taboola.com>
2019-10-10 14:42:44 -0400

    remove nullFields

--- backend/utils/DataLoader.js
+++ backend/utils/DataLoader.js
@@ -1,5 +1,4 @@
 const DataLoader = require('dataloader')
-const NullFields = require('./nullFields')
 const PubCrawlDB = require('./PubCrawlDB')
 
 const makeDataLoaders = (pubId, pubName) => {
@@ -7,7 +6,6 @@ const makeDataLoaders = (pubId, pubName) => {
 		audit: new DataLoader(async ids => {
 			try {
 				const res = await PubCrawlDB.getCrawlerAuditData(pubName, ids)
-				if (!res.find) return ids.map(_ => NullFields.audit)
 
 				return ids.map(id => res.find(auditData => auditData.pub_item_id === id))
 			} catch (error) {
@@ -17,7 +15,6 @@ const makeDataLoaders = (pubId, pubName) => {
 		instructions: new DataLoader(async ids => {
 			try {
 				const res = await PubCrawlDB.getCrawlerInstructionsData(pubName, ids)
-				if (!res.find) return ids.map(_ => NullFields.audit)
 
 				return ids.map(id =>
 					res.find(instructionsData => instructionsData.pub_item_id === id)
@@ -29,7 +26,6 @@ const makeDataLoaders = (pubId, pubName) => {
 		channels: new DataLoader(async ids => {
 			try {
 				const res = await PubCrawlDB.getChannelsData(pubId, ids)
-				if (!res.find) return ids.map(_ => NullFields.channels)
 
 				return ids.map(id => res.filter(channelData => channelData.video_id === id))
 			} catch (error) {
