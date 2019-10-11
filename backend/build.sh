1b2428616b7684b5939d0f49371a12cb0bd94897
mark.sauer.utley <mark.sauer.utley@taboola.com>
2019-10-11 11:30:53 -0400

    update Dockerfile

--- backend/build.sh
+++ backend/build.sh
@@ -1,2 +1,14 @@
-$HOME/.yarn/bin/yarn install --pure-lockfile
-exec "$@"
+if [ ! -f .yarn-cache.tgz ]; then
+  echo "Init empty .yarn-cache.tgz"
+  tar cvzf .yarn-cache.tgz --files-from /dev/null
+fi
+​
+docker build . -t yarn-demo
+​
+docker run --rm --entrypoint cat yarn-demo:latest /tmp/yarn.lock > /tmp/yarn.lock
+if ! diff -q yarn.lock /tmp/yarn.lock > /dev/null  2>&1; then
+  echo "Saving Yarn cache"
+  docker run --rm --entrypoint tar yarn-demo:latest czf - /root/.yarn-cache/ > .yarn-cache.tgz
+  echo "Saving yarn.lock"
+  cp /tmp/yarn.lock yarn.lock
+fi
