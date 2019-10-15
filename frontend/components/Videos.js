3ab8dea6a1284b9ce901ede81b53ad58f9ddfadc
mark.sauer.utley <mark.sauer.utley@taboola.com>
2019-10-15 17:16:26 -0400

    fix cell creation background shading logic

--- frontend/components/Videos.js
+++ frontend/components/Videos.js
@@ -1,98 +1,101 @@
-import styled from 'styled-components'
-import { nestedProperties } from '../lib/videoFilters'
+import styled from 'styled-components';
+import { nestedProperties } from '../lib/videoFilters';
 
-export default props => {
-	const makeCell = ({ key, value, isEven }) => {
-		return (
-			<div
-				className={isEven ? 'cell even' : 'cell odd'}
-				key={`${key}:${value}${Math.floor(Math.random() * 100)}`}
-			>
-				<span className="key">{key}:</span>
-				<span>{value ? value.toString() : '(null)'}</span>
-			</div>
-		)
-	}
+export default (props) => {
+	const makeCell = (function() {
+		let counter = 0;
+		return ({ key, value }) => {
+			counter += 1;
+			const isEven = counter % 2 === 0;
+
+			return (
+				<div
+					className={isEven ? `cell even` : `cell odd`}
+					key={`${key}:${value}${Math.floor(Math.random() * 100)}`}
+				>
+					<span className="key">{key}:</span>
+					<span>{value ? value.toString() : '(null)'}</span>
+				</div>
+			);
+		};
+	})();
 
 	const mappedVids = props.videos.map((video, idx) => {
-		const isEven = idx % 2 === 0
+		const isEven = idx % 2 === 0;
 		return (
 			<VideoResult
 				key={Math.floor(Math.random() * 1000000)}
 				className={isEven ? 'even' : 'odd'}
 			>
 				{Object.keys(video).map((property, idx) => {
-					const isEven = idx % 2 === 0
-
 					if (property === 'channelsData') {
-						const channelContainer = video.channelsData.map(
-							(channel, channelIdx) => {
-								const channelIsEven = channelIdx % 2 === 0
-								const channelCells = Object.keys(channel).map(
-									channelProperty => {
-										return makeCell({
-											key: channelProperty,
-											value: channel[channelProperty],
-											isEven: true,
-										})
-									}
-								)
-								if (!channelCells.length) return
-
-								return (
-									<ChannelContainer
-										key={Math.floor(Math.random() * 10000)}
-										className={channelIsEven ? 'even' : 'odd'}
-									>
-										{channelCells}
-									</ChannelContainer>
-								)
-							}
-						)
-
 						// a check to see if there are any channels with active filters
-						const shouldShowChannelsData = !!Object.keys(
-							video.channelsData[0] || {}
-						).length
+						const numChannels = Object.keys(video.channelsData[0] || {}).length;
+
+						// if not, return nothing
+						if (numChannels === 0) return null;
+
+						const channelContainer = video.channelsData.map((channel, channelIdx) => {
+							const channelIsEven = channelIdx % 2 === 0;
+							const channelCells = Object.keys(channel).map((key) => {
+								return makeCell({
+									key,
+									value: channel[key],
+								});
+							});
+
+							if (!channelCells.length) return;
+
+							return (
+								<ChannelContainer
+									key={Math.floor(Math.random() * 10000)}
+									className={channelIsEven ? 'even' : 'odd'}
+								>
+									{channelCells}
+								</ChannelContainer>
+							);
+						});
 
 						return (
 							<div key={Math.floor(Math.random() * 10000)}>
-								{shouldShowChannelsData && <h3>Channel Data</h3>}
+								<h3>Channel Data</h3>
 								{channelContainer}
 							</div>
-						)
+						);
 					}
 
 					if (nestedProperties.includes(property)) {
 						return Object.keys(video[property]).map((nestedProp, idx) => {
-							const nestedIsEven = isEven ? idx % 2 === 0 : idx % 2 === 1
 							return makeCell({
 								key: nestedProp,
 								value: video[property][nestedProp],
-								isEven: nestedIsEven,
-							})
-						})
+							});
+						});
 					}
 
-					return makeCell({ key: property, value: video[property], isEven })
+					return makeCell({
+						key: property,
+						value: video[property],
+						isEven,
+					});
 				})}
 			</VideoResult>
-		)
-	})
+		);
+	});
 
-	return <div>{mappedVids}</div>
-}
+	return <div>{mappedVids}</div>;
+};
 
 const VideoResult = styled.div`
 	margin: 50px 0;
 	padding-bottom: 50px;
 
 	&.even {
-		border-bottom: 4px solid ${props => props.theme.grey};
+		border-bottom: 4px solid ${(props) => props.theme.grey};
 	}
 
 	&.odd {
-		border-bottom: 4px solid ${props => props.theme.blue};
+		border-bottom: 4px solid ${(props) => props.theme.blue};
 	}
 
 	div.cell {
@@ -102,7 +105,7 @@ const VideoResult = styled.div`
 		word-break: break-word;
 
 		&.even {
-			background-color: ${props => props.theme.lightgrey};
+			background-color: ${(props) => props.theme.lightgrey};
 		}
 	}
 	span.key {
@@ -112,9 +115,9 @@ const VideoResult = styled.div`
 
 	h3 {
 		margin: 5px 0 0;
-		color: ${props => props.theme.blue};
+		color: ${(props) => props.theme.blue};
 	}
-`
+`;
 
 const ChannelContainer = styled.div`
 	div.cell.even,
@@ -122,9 +125,9 @@ const ChannelContainer = styled.div`
 		background-color: transparent;
 	}
 	&.even {
-		background-color: ${props => props.theme.lightgrey};
+		background-color: ${(props) => props.theme.lightgrey};
 	}
 	&.odd {
 		background-color: white;
 	}
-`
+`;
