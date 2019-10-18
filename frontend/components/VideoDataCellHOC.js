24dd11d4828a8feb492549c83ec08296278a2162
mark.sauer.utley <mark.sauer.utley@taboola.com>
2019-10-18 16:40:43 -0400

    make links a-tags

--- frontend/components/VideoDataCellHOC.js
+++ frontend/components/VideoDataCellHOC.js
@@ -1,3 +1,20 @@
+import styled from 'styled-components';
+
+
+const LinkStyles = styled.a`
+	color: ${(props) => props.theme.darkblue};
+`;
+
+const isUrl = (string) => {
+	try {
+		// eslint-disable-next-line no-new
+		new URL(string);
+		return true;
+	} catch (error) {
+		return false;
+	}
+};
+
 const makeCellComponent = () => {
 	let counter = 0;
 
@@ -5,13 +22,29 @@ const makeCellComponent = () => {
 		counter += 1;
 		const isCellEven = counter % 2 === 0;
 
+		const valueSwitch = () => {
+			switch (true) {
+			case isUrl(value): {
+				const stringVal = value.toString();
+
+				return <LinkStyles href={stringVal} target="_blank" rel="noopener noreferrer">{stringVal}</LinkStyles>;
+			}
+			case !!value: {
+				return value.toString();
+			}
+			default: {
+				return '(null)';
+			}
+			}
+		};
+
 		return (
 			<div
 				className={isCellEven ? 'cell even' : 'cell odd'}
 				key={`${displayKey}:${value}${Math.floor(Math.random() * 100)}`}
 			>
 				<span className="key">{displayKey}:</span>
-				<span>{value ? value.toString() : '(null)'}</span>
+				<span>{valueSwitch()}</span>
 			</div>
 		);
 	};
